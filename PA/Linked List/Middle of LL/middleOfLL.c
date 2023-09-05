a5f8ba48dcc9afc0106d510de66778ed8ce1c75f
itsdhruvarora <dhruv2073.be21@chitkara.edu.in>
2023-09-05 21:29:57 +0530

    added Circular Queue

--- PA/Linked List/Middle of LL/middleOfLL.c
+++ PA/Linked List/Middle of LL/middleOfLL.c
@@ -21,8 +21,7 @@ static int findMid(struct Node* head)
 
 void push(struct Node** head_ref, int new_data)
 {
-	struct Node* new_node
-		= (struct Node*)malloc(sizeof(struct Node));
+	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
 	new_node->data = new_data;
 	new_node->next = (*head_ref);
 	(*head_ref) = new_node;
